9027f741e1002d459d4c12e64168becc5cafa247
Link <956751855@qq.com>
2024-04-15 20:34:03 +0800

    build component: Seach

---
+++ src/api.js
@@ -0,0 +1,8 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6d91aa94d4mshc389c6d24a8c3efp1fb783jsn245ddbee9e88',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
---
+++ src/components/search/search.js
@@ -0,0 +1,39 @@
+import {AsyncPaginate} from 'react-select-async-paginate';
+import {useState} from 'react';
+import {geoApiOptions,GEO_API_URL} from '../../api';
+const Search = ({onSearchChange}) => {
+
+    const [search, setSearch] = useState(null);
+
+    const handleOnChange = (searchData) => {
+        // 搜索框中选择的城市的数据
+        setSearch(searchData);
+        onSearchChange(searchData); 
+    }
+
+    const loadOptions = async (inputValue) => {
+        return fetch(`${GEO_API_URL}?minPopulation=10000&namePrefix=${inputValue}`,geoApiOptions)
+        .then(response => response.json())
+        .then( (response) => {
+            return {
+                options: response.data.map((city) => {
+                    return {
+                        value: `${city.latitude},${city.longitude}`,
+                        label: `${city.city}, ${city.countryCode}`
+                    }
+                })
+            }
+        })
+        .catch(err => console.error(err));
+    }
+
+    return(
+        <AsyncPaginate 
+            placeholder="Search for city"
+            debounceTimeout={600}
+            value={search}
+            onChange={handleOnChange}
+            loadOptions={loadOptions} // function to load options, Array
+    />)
+}
+export default Search;
